fb7f8c65ef00d2d38e4266d48db5c679e3360338
Diekkan <merentiel.diego@gmail.com>
2022-02-10 10:52:28 -0800

    Bubble sort fixed and betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+
+/**
+ * bubble_sort - Bubble sort algorithm.
+ *@array: array to sort
+ *@size: elements of the array
+ */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t n = size, i;
 	int aux, swap;
 
-	do
-	{
+	do {
 		swap = 0;
 		for (i = 1; i < n; i++)
 		{
@@ -17,9 +23,9 @@ void bubble_sort(int *array, size_t size)
 				array[i - 1] = array[i];
 				array[i] = aux;
 				swap = 1;
-                print_array(array, size);
+				print_array(array, size);
 			}
 		}
 		n = n - 1;
-	}while (swap != 0);
+	} while (swap != 0);
 }
