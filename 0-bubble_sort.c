7435c1ec330385be1e4a010bbd2b4e1e3cc92f8b
Santi3948 <santiagonegreyra@gmail.com>
2022-02-10 09:37:32 -0800

    0. Bubble sort facheiro

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+void bubble_sort(int *array, size_t size)
+{
+	size_t n = size, i;
+	int aux, swap;
+
+	do
+	{
+		swap = 0;
+		for (i = 1; i < n; i++)
+		{
+			if (array[i - 1] > array[i])
+			{
+				aux  = array[i - 1];
+				array[i - 1] = array[i];
+				array[i] = aux;
+				swap = 1;
+                print_array(array, size);
+			}
+		}
+		n = n - 1;
+	}while (swap != 0);
+}
