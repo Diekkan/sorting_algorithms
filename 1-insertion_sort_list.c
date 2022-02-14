f7b6c6f4680180373329f075c3edc96899c1623b
Diekkan <merentiel.diego@gmail.com>
2022-02-14 11:10:09 -0800

    3 work locally

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,56 +1,35 @@
 #include "sort.h"
 #include <stdio.h>
-/**
- * insertion_sort_list - Insertion sort algorithm.
- *@list: doubly linked list.
- */
 
+/**
+ *
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *laux = NULL, *aux2 = NULL;
-	listint_t *swap1 = NULL, *swap2 = NULL, *swap3 = NULL;
+	listint_t *aux = NULL, *aux2 = NULL, *aux0 = NULL;
+	int flag;
 
-	if (!*list)
+	if (!list || !*list || !(*list)->next)
 		return;
-	laux = *list;
-	while (laux->next)
+	aux = (*list)->next;
+	while(aux)
 	{
-		if (laux->n > laux->next->n)
+		flag = 0;
+		while (aux->prev && aux->prev->n > aux->n)
 		{
-			swap1 = laux, swap2 = laux->next, swap3 = laux->prev;
-			if (swap2->next)
-			{
-				swap1->next->prev = swap1;
-			}
-			swap1->next = swap2->next;
-			swap1->prev = laux->next;
-			swap2->prev = swap3;
-			swap2->next = swap1;
-			swap3->next = swap2;
+			flag = 1;
+			aux2 = aux->next;
+			aux0 = aux->prev;
+			aux->prev->next = aux2;
+			aux->next = aux->prev;
+			if (aux->next)
+				aux->next->prev = aux->prev;
+			aux->prev = aux->prev->prev;
+			aux->prev->prev->next = aux;
+			print_list(*list);
+			if (flag == 1)
+				break;
 		}
-		printf("1er while: ");
-		print_list(*list);
-		aux2 = laux;
-		while (aux2)
-		{
-			if (aux2->prev)
-				if (aux2->n < aux2->prev->n)
-				{
-				swap1 = aux2, swap2 = aux2->next, swap3 = aux2->prev;
-				if (swap3->prev)
-				{
-					swap3->prev->next = swap1;
-				}
-				swap1->prev = swap3->prev;
-				swap1->next = swap3;
-				swap3->next = swap2;
-				swap3->prev = swap1;
-				swap2->prev = swap3;
-			}
-			aux2 = aux2->prev;
-		}
-		laux = laux->next;
-		printf("2ndo while: ");
-		print_list(*list);
+		aux = aux->next;
 	}
 }
