30c9c49ad787fc73dbe3b02ce2cf7e623364d79b
Diekkan <3950@holbertonschool.com>
2022-02-15 12:12:43 -0300

    1. Insertion sort list fixed

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,35 +1,38 @@
 #include "sort.h"
-#include <stdio.h>
-
 /**
- *
+ * insertion_sort_list - insertion sort algorithm
+ * @list: head position of list
  */
+
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *aux = NULL, *aux2 = NULL, *aux0 = NULL;
-	int flag;
+	listint_t *aux = *list;
+	listint_t *tmp = NULL;
 
-	if (!list || !*list || !(*list)->next)
+	if (!list || !*list || (*list)->next == NULL)
 		return;
-	aux = (*list)->next;
-	while(aux)
+	while (aux)
 	{
-		flag = 0;
-		while (aux->prev && aux->prev->n > aux->n)
+		tmp = aux->next;
+		while (tmp && tmp->prev && tmp->n < tmp->prev->n)
 		{
-			flag = 1;
-			aux2 = aux->next;
-			aux0 = aux->prev;
-			aux->prev->next = aux2;
-			aux->next = aux->prev;
-			if (aux->next)
-				aux->next->prev = aux->prev;
-			aux->prev = aux->prev->prev;
-			aux->prev->prev->next = aux;
+			if (tmp->prev != NULL)
+				tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->next->prev;
+			if (tmp->prev != NULL)
+				tmp->prev->next = tmp;
+			if (tmp->prev == NULL)
+				*list = tmp;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp;
 			print_list(*list);
-			if (flag == 1)
-				break;
 		}
-		aux = aux->next;
+		if (!aux->next)
+			break;
+		if (aux->n < aux->next->n)
+			aux = aux->next;
 	}
 }
